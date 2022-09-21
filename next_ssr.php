eeafecf3459808888acf5f7264f6d1695fec620b
NesarAhmedRazon <grim.roy@gmail.com>
2022-09-21 16:59:15 +0600

    first commit

---
+++ next_ssr.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+*
+* @package NextSSR
+*
+* Plugin Name: Next SSR
+* Plugin URI: https://github.com/NesarAhmedRazon/next_ssr
+* Description: This plugin will add Features for NextJs Server Side Randaring.
+* Author: Nesar Ahmed
+* Version: 0.0.1
+* Author URI: https://github.com/NesarAhmedRazon/
+* Text Domain: next_ssr
+*/
+
+define(
+    'NEXT_SSR',
+    __FILE__
+);
